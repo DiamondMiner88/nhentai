f142ca68dc782e631c96a6afee022b6ed97b6bb1
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-01-11 14:52:30 -0800

    feat: preserve raw search result

--- src/search.ts
+++ src/search.ts
@@ -16,9 +16,15 @@ export default class SearchResult {
      */
     readonly doujinsPerPage: number;
 
+    /**
+     * Raw response from the API
+     */
+    readonly raw?: APISearchResult;
+
     constructor(searchresult: APISearchResult, api: API) {
         this.doujins = searchresult.result.map(doujin => new Doujin(doujin, api));
         this.numPages = searchresult.num_pages;
         this.doujinsPerPage = searchresult.per_page;
+        if (api.options.preserveRaw) this.raw = searchresult;
     }
 }

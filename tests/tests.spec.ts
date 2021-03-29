73aa1f9c6f13604244b810f785bff87565ec58ae
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-03-29 12:52:49 -0700

    test: improve tests
    - Better descriptions
    - Add test for `API#doujinExists()`

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,82 +1,79 @@
 import { use as chaiUse, expect } from 'chai';
 import * as chai_as_promised from 'chai-as-promised';
 import 'mocha';
+import Image from '../src/image';
 chaiUse(chai_as_promised);
 
 import { API } from '../src/index';
-import Image from '../src/image';
 import SearchResult from '../src/search';
 
 const api = new API();
 
-// does not fucking work, whatever the fuck i'm doing does not work
-// both pass even though they shouldnt, i reversed the fucking values
-// describe('doujinExists', () => {
-//     it('should return false', () => {
-//         expect(api.doujinExists(9999999)).to.eventually.be.true;
-//     });
-//     it('should return true', () => {
-//         expect(api.doujinExists(334430)).eventually.to.be.false;
-//     });
-// });
+describe('doujinExists', () => {
+    it("return false when doujin doesn't exist", () => {
+        return expect(api.doujinExists(9999999)).to.eventually.be.false;
+    });
+    it('return true when doujin exists', () => {
+        return expect(api.doujinExists(334430)).eventually.to.be.true;
+    });
+});
 
 describe('fetchDoujin', () => {
-    it('should return a doujin', () => {
+    it('returns a doujin', () => {
         return expect(api.fetchDoujin(334430)).eventually.be.not.undefined;
     });
-    it('should reject if page is not a number', () => {
+    it('rejects when page is not number', () => {
         return expect(api.fetchDoujin('NaN')).to.be.rejectedWith('DoujinID paramater is not a number.');
     });
-    it('should reject if doujin does not exist', () => {
+    it('non-existing doujin fetch returns undefined', () => {
         return expect(api.fetchDoujin(0)).to.eventually.be.undefined;
     });
-    it('should reject if doujinID is lower than 0', () => {
+    it('rejects when doujin id is <1', () => {
         return expect(api.fetchDoujin(-1)).to.be.rejectedWith('DoujinID cannot be lower than 1.');
     });
-    it('should return a buffer of the thumbnail', async () => {
+    it('return thumb image buffer', async () => {
         const doujin = await api.fetchDoujin(334430);
         return expect(doujin?.thumbnail.fetch()).to.eventually.be.an.instanceOf(Buffer);
     });
 });
 
 describe('fetchHomepage', () => {
-    it('should return a search result', () => {
+    it('returns search results', () => {
         return expect(api.fetchHomepage()).to.eventually.be.an.instanceOf(SearchResult);
     });
 });
 
 describe('search', () => {
-    it('should return a search result', async () => {
+    it('returns search results', async () => {
         const result: SearchResult = await api.search('METAMORPHOSIS');
         return expect(result.doujins[0].pages[0]).to.be.an.instanceOf(Image);
     });
 });
 
 describe('searchByTagID', () => {
-    it('should return a search result', async () => {
+    it('returns search results', async () => {
         const result: SearchResult = await api.searchByTagID(8739);
         return expect(result.doujins[0].pages[0]).to.be.an.instanceOf(Image);
     });
-    it('should catch an api error', () => {
+    it('reject because of api error', () => {
         return expect(api.searchByTagID(1, 1, 'a')).to.eventually.be.rejectedWith('API returned an error');
     });
 });
 
 describe('searchRelated', () => {
-    it('should return a search result', async () => {
-        const result: SearchResult = await api.searchRelated(334430);
-        return expect(result.doujins[0].pages[0]).to.be.an.instanceOf(Image);
+    it('returns search results', () => {
+        return expect(api.searchRelated(334430)).to.eventually.be.an.instanceOf(SearchResult);
     });
 });
 
 describe('randomDoujinID', () => {
-    it('should return a number', async () => {
+    it('returns a doujin id', () => {
         return expect(api.randomDoujinID()).to.eventually.be.a('number');
     });
 });
 
 describe('randomDoujin', () => {
-    it('should return a Doujin', () => {
+    it('returns a doujin', () => {
         return expect(api.randomDoujin()).to.eventually.be.fulfilled;
     });
 });

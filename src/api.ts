77f600dab1da6e259fb3947f1d9ad7bd45eed1da
Turbo <19479111+TurboCheetah@users.noreply.github.com>
2021-08-20 20:51:29 -0500

    feat: Add language search option (#10)
    Co-authored-by: Diamond <33725716+DiamondMiner88@users.noreply.github.com>

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,12 @@ import { Doujin } from './doujin';
 import { SearchResult } from './search';
 import { API_URL, HOST_URL, SortMethods, SortValues } from './constants';
 
+export interface APISearchOptions {
+    page?: number;
+    sort?: SortMethods;
+    language?: 'english' | 'japanese' | 'chinese';
+}
+
 export class API {
     /**
      * Constuct a new API wrapper
@@ -66,58 +72,50 @@ export class API {
 
     /**
      * Fetch homepage doujins
+     * @param options Search Options
      */
-    fetchHomepage(page: string | number = 1, sort = SortMethods.RECENT): Promise<SearchResult> {
-        return this.search('*', page, sort);
+    fetchHomepage(options: APISearchOptions = {}): Promise<SearchResult> {
+        return this.search('*', options);
     }
 
     /**
      * Search by string query
+     * @param options Search Options
      */
-    async search(query: string, page: string | number = 1, sort = SortMethods.RECENT): Promise<SearchResult> {
-        page = Number(page);
+    async search(query: string, options: APISearchOptions = {}): Promise<SearchResult> {
+        options.page = Number(options.page);
 
-        if (isNaN(page)) throw new TypeError('page is not a number');
-        if (!SortValues.includes(sort)) throw new TypeError('sort method is not valid');
+        if (options.page && isNaN(options.page)) throw new TypeError('page is not a number');
+        if (options.sort && !SortValues.includes(options.sort)) throw new TypeError('sort method is not valid');
 
-        const res = await this.fetch(`/galleries/search?query=${query}&page=${page}&sort=${sort}`);
+        // prettier-ignore
+        const res = await this.fetch(`/galleries/search?query=${query}${options.language ? ` ${options.language}` : ''}&page=${options.page || '1'}&sort=${options.sort || SortMethods.RECENT}`);
         return new SearchResult(res as APISearchResult);
     }
 
     /**
      * Search doujins with this tag
      * @param id Tag ID
-     * @param page Page number
-     * @param sort API sort method
+     * @param options Search Options
      */
-    async searchByTagID(
-        id: number | string,
-        page: string | number = 1,
-        sort = SortMethods.RECENT
-    ): Promise<SearchResult> {
-        id = Number(id);
-        page = Number(page);
-
+    async searchByTagID(id: number, options: APISearchOptions = {}): Promise<SearchResult> {
         if (isNaN(id)) throw new TypeError('tagId is not a number');
-        if (isNaN(page)) throw new TypeError('page is not a number');
-        if (!SortValues.includes(sort)) throw new TypeError('sort method is not valid');
+        if (options.page && isNaN(options.page)) throw new TypeError('page is not a number');
+        if (options.sort && !SortValues.includes(options.sort)) throw new TypeError('sort method is not valid');
 
         // An empty &sort query param causes an error
-        const res = await this.fetch(`/galleries/tagged?tag_id=${id}&page=${page}${sort ? `&sort=${sort}` : ''}`);
+        // prettier-ignore
+        const res = await this.fetch(`/galleries/tagged?tag_id=${id}&page=${options.page || '1'}${options.sort ? `&sort=${options.sort}` : ''}`);
         return new SearchResult(res as APISearchResult);
     }
 
     /**
      * Find similar doujins
      * @param id Doujin ID
-     * @param page Page number
      */
-    async searchRelated(id: number | string, page: string | number = 1): Promise<SearchResult> {
+    async searchRelated(id: number | string): Promise<SearchResult> {
         id = Number(id);
-        page = Number(page);
-
         if (isNaN(id)) throw new TypeError('doujinID is not a number');
-        if (isNaN(page)) throw new TypeError('page is not a number');
 
         const res = await this.fetch(`/gallery/${id}/related`);
         return new SearchResult(res as APISearchResult);
